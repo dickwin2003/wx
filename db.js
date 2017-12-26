75fa7bc1b99895ebdd5e0ad5be07a70609e4b57e
dickwin2003 <dickwin2003@gmail.com>
2017-12-26 09:18:06 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-    host : '111.230.29.226',
-    user : 'node',
-    password : 'Wdmms123@',
-    database:'nodejs_db'
+    host : '111.230.29.111',
+    user : 'xxx',
+    password : 'xxxx@',
+    database:'xxxx'
 })
 
 module.exports = conn;
